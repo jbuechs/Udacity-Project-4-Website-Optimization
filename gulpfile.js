f4b2f4e2a09dac7d57b155488918361fdce7ea70
jbuechs <janina.buechner@gmail.com>
2015-07-24 09:51:11 -0400

    Update gulp to work on views directory

--- gulpfile.js
+++ gulpfile.js
@@ -11,6 +11,9 @@ gulp.task('scripts', function() {
 	gulp.src('js/*.js')
 		.pipe(uglify())
 		.pipe(gulp.dest('dist/js'));
+	gulp.src('views/js/*.js')
+		.pipe(uglify())
+		.pipe(gulp.dest('dist/views/js'));
 });
 
 // Minifies the css and outputs to dist/css/*.css
@@ -18,25 +21,39 @@ gulp.task('styles', function(){
 	gulp.src('css/*.css')
 		.pipe(minifyCSS())
 		.pipe(gulp.dest('dist/css'));
+	gulp.src('views/css/*.css')
+		.pipe(minifyCSS())
+		.pipe(gulp.dest('dist/views/css'));
 });
 
 // Minifies the images and outputs them to dist/img
 gulp.task('imagemin', function(){
-	return gulp.src('img/*')
+	gulp.src('img/*')
 		.pipe(imagemin({
 			progressive: true,
 			svgoPlugins: [{removeViewBox: false}],
             use: [pngquant()]
 		}))
 		.pipe(gulp.dest('dist/img'));
+	gulp.src('views/img/*')
+		.pipe(imagemin({
+			progressive: true,
+			svgoPlugins: [{removeViewBox: false}],
+            use: [pngquant()]
+		}))
+		.pipe(gulp.dest('dist/views/img'));
 });
 
 // Minifies the HTML, minifies inline JS and CSS, outputs it to dist/*.html
 gulp.task('content', function() {
-    return gulp.src('*.html')
+    gulp.src('*.html')
         .pipe(minifyhtml())
         .pipe(minifyInline())
         .pipe(gulp.dest('dist/'));
+    gulp.src('views/*.html')
+        .pipe(minifyhtml())
+        .pipe(minifyInline())
+        .pipe(gulp.dest('dist/views/'));
 });
 
 // Watches for changes in files
